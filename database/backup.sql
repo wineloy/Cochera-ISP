a1f1cb0dbcc80a10838184ddbfb1e58dc731f041
wineloy <wineloy@gmail.com>
2020-11-18 15:33:44 -0600

    add file database

---
+++ database/backup.sql
@@ -0,0 +1,247 @@
+-- MariaDB dump 10.17  Distrib 10.4.14-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: WISPCH
+-- ------------------------------------------------------
+-- Server version	10.4.14-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `carrito`
+--
+
+DROP TABLE IF EXISTS `carrito`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `carrito` (
+  `idCarrito` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) NOT NULL,
+  `idOferta` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  PRIMARY KEY (`idCarrito`),
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idOferta` (`idOferta`),
+  CONSTRAINT `carrito_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`),
+  CONSTRAINT `carrito_ibfk_2` FOREIGN KEY (`idOferta`) REFERENCES `ofertas` (`idOferta`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carrito`
+--
+
+LOCK TABLES `carrito` WRITE;
+/*!40000 ALTER TABLE `carrito` DISABLE KEYS */;
+/*!40000 ALTER TABLE `carrito` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categorias` (
+  `idCategoria` int(11) NOT NULL AUTO_INCREMENT,
+  `Categoria` text DEFAULT NULL,
+  `descripcion` text DEFAULT NULL,
+  PRIMARY KEY (`idCategoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,'GAMER','Menor ping o latencia para mejorar tu jugabilidad y desempeño '),(2,'BÁSICO ','Enfocado para realizar tus tareas cotidianas '),(3,'EMPRESAS ','Soporte a la medida a las empresas redundante'),(4,'HOGAR','Enfocado para familias con múltiples integrantes ');
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes` (
+  `idCliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(250) NOT NULL,
+  `apellidos` varchar(250) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `telefono` varchar(16) DEFAULT NULL,
+  `sexo` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`idCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `direcciones`
+--
+
+DROP TABLE IF EXISTS `direcciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `direcciones` (
+  `idDireccion` int(11) NOT NULL AUTO_INCREMENT,
+  `idCliente` int(11) NOT NULL,
+  `direccion` text NOT NULL,
+  `referencia` text NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `numerointerno` varchar(10) DEFAULT NULL,
+  `entrecalles` varchar(20) DEFAULT NULL,
+  `localidad` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`idDireccion`),
+  KEY `idCliente` (`idCliente`),
+  CONSTRAINT `direcciones_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `clientes` (`idCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `direcciones`
+--
+
+LOCK TABLES `direcciones` WRITE;
+/*!40000 ALTER TABLE `direcciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `direcciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `formaspago`
+--
+
+DROP TABLE IF EXISTS `formaspago`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `formaspago` (
+  `idPago` int(11) NOT NULL AUTO_INCREMENT,
+  `formaPago` varchar(100) NOT NULL,
+  PRIMARY KEY (`idPago`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `formaspago`
+--
+
+LOCK TABLES `formaspago` WRITE;
+/*!40000 ALTER TABLE `formaspago` DISABLE KEYS */;
+/*!40000 ALTER TABLE `formaspago` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ofertas`
+--
+
+DROP TABLE IF EXISTS `ofertas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ofertas` (
+  `idOferta` int(11) NOT NULL AUTO_INCREMENT,
+  `idPaquete` int(11) NOT NULL,
+  `porcentaje` decimal(10,0) NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  PRIMARY KEY (`idOferta`),
+  KEY `idPaquete` (`idPaquete`),
+  CONSTRAINT `ofertas_ibfk_1` FOREIGN KEY (`idPaquete`) REFERENCES `paquetes` (`idPaquete`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ofertas`
+--
+
+LOCK TABLES `ofertas` WRITE;
+/*!40000 ALTER TABLE `ofertas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ofertas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paquetes`
+--
+
+DROP TABLE IF EXISTS `paquetes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paquetes` (
+  `idPaquete` int(11) NOT NULL AUTO_INCREMENT,
+  `idCategoria` int(11) NOT NULL,
+  `nombrepaquete` varchar(100) DEFAULT NULL,
+  `megas` varchar(100) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL,
+  `imagen` varchar(100) DEFAULT NULL,
+  `precio` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`idPaquete`),
+  KEY `idCategoria` (`idCategoria`),
+  CONSTRAINT `paquetes_ibfk_1` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paquetes`
+--
+
+LOCK TABLES `paquetes` WRITE;
+/*!40000 ALTER TABLE `paquetes` DISABLE KEYS */;
+INSERT INTO `paquetes` VALUES (1,1,'ELRUBIOS','20','enfocado a los Streamings',NULL,600),(2,1,'PLAY','10','Enfocado al gamer casual',NULL,450),(3,3,'Business initial','40','Enfocado a pequeñas empresas',NULL,1500),(4,3,'Premiun','100','Enfocado a empresas medianas ',NULL,4000),(5,4,'Family ','15','Enfocado a la familia ',NULL,350),(6,4,'Series And Movies','20','Enfocado para los cinefilos',NULL,500),(7,2,'Ofimatica','5','Funciones basicas',NULL,300);
+/*!40000 ALTER TABLE `paquetes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `idCliente` int(11) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `contraseña` text NOT NULL,
+  PRIMARY KEY (`idUsuario`),
+  KEY `idCliente` (`idCliente`),
+  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `clientes` (`idCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-18 15:21:20
