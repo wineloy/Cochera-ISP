cc1d2dae731f25dbd1f1d6af804fc0f601bdc726
wineloy <wineloy@gmail.com>
2021-01-19 19:19:11 -0600

    add procedimiento almacenado para consulta en form de compra

---
+++ database/StoreProcedure/datosFormularioCompra.sql
@@ -0,0 +1,11 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `datosFormularioCompra`(
+in _email varchar(250)
+)
+BEGIN
+ if exists(select 1 from usuarios where email = _email ) = 1 then
+	select C.nombre, C.apellidos, C.telefono from clientes C inner join usuarios U on (U.idCliente=C.idCliente)
+    where U.email= _email LIMIT 1;
+ else
+	select -1 ;# no existe este correo;
+ end if;
+END
