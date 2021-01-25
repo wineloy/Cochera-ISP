5246c70359c83c74cd550a1a766c983474936e5c
wineloy <wineloy@gmail.com>
2021-01-24 18:31:47 -0600

    Procedimiento almacenado y diagrama de base de datos

--- database/StoreProcedure/datosFormularioCompra.sql
+++ database/StoreProcedure/datosFormularioCompra.sql
@@ -3,7 +3,7 @@ in _email varchar(250)
 )
 BEGIN
  if exists(select 1 from usuarios where email = _email ) = 1 then
-	select C.nombre, C.apellidos, C.telefono from clientes C inner join usuarios U on (U.idCliente=C.idCliente)
+	select C.nombre, C.apellidos, C.telefono, U.email from clientes C inner join usuarios U on (U.idCliente=C.idCliente)
     where U.email= _email LIMIT 1;
  else
 	select -1 ;# no existe este correo;
