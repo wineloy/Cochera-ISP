7ee5dd102af5c23e8eef533eb767dccbf8c84f56
wineloy <wineloy@gmail.com>
2021-01-25 20:28:32 -0600

    add actualizar precio descuento paquetes

---
+++ include/updatePaquetes.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once "conexion.php";
+
+class Precio
+{
+
+    private $resultado;
+    private $conexion;
+
+    public function __construct(ConexionSql $conexion)
+    {
+        $this->conexion = $conexion;
+        $this->conexion->Conectar();
+        $this->resultado = $conexion->ConsultaSql("call getPaquetes();");
+    }
+
+    public function CalculaDescuento()
+    {
+        $paquetes = $this->resultado;
+        for ($package = 0; $package < count($paquetes); $package++) {
+            $idPaquete = $paquetes[$package][0];
+            $descuento = $paquetes[$package][5];
+
+            if ($paquetes[$package][7] == 1) {
+                
+                $descuento = $paquetes[$package][5] - ( ($paquetes[$package][5] * $paquetes[$package][8])/100 );
+                $this->conexion->ConsultaSql("UPDATE `paquetes` SET `descuento` = $descuento WHERE (`idPaquete` = $idPaquete);");
+            }
+            else{
+                $this->conexion->ConsultaSql("UPDATE `paquetes` SET `descuento` = $descuento  WHERE (`idPaquete` = $idPaquete);");
+            }
+              
+        }
+    }
+
+    
+}
+$Precio = new Precio($conexion);
+echo $Precio->CalculaDescuento();
