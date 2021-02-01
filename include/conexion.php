318e30a3fa4f6e7c2c5fa20b022bcb6b1014cd20
wineloy <wineloy@gmail.com>
2021-01-31 23:50:25 -0600

    variable de entorno

--- include/conexion.php
+++ include/conexion.php
@@ -71,7 +71,7 @@ class ConexionSql{
 
 
 // Esto se hace para heredar la conexion con env
-if(isset($_ENV["DB_HOST"]) && isset($_ENV["DB_USER"]) && isset($_ENV["DB_PASSWORD"]) && isset($_ENV["DB_NAme"])){
+if(isset($_ENV["DB_HOST"]) && isset($_ENV["DB_USER"]) && isset($_ENV["DB_PASSWORD"]) && isset($_ENV["DB_NAME"])){
 	$conexion = new ConexionSql($_ENV["DB_HOST"],$_ENV["DB_USER"], $_ENV["DB_PASSWORD"],$_ENV["DB_NAme"]);
 }else{
 	$conexion = new ConexionSql("localhost","root", "","WISPCH");
